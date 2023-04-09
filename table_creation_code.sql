d3231e74ab7fcaa819dfbd94a399bf3dc32e3bee
Shubhashree Munot <120332097+Shubhashree26@users.noreply.github.com>
2023-04-09 19:21:40 +0530

    updated the file

--- table_creation_code.sql
+++ table_creation_code.sql
@@ -1,3 +1,15 @@
+/*
+Two tables need to be created for loading the data , first one is OLYMPICS_HISTORY and second one is OLYMPICS_HISTORY_NOC_REGIONS.
+1.  OLYMPICS_HISTORY table is loaded with data from the file athlete_events.csv which contains 271116 rows and 15 columns. Each row corresponds to an
+    individual athlete competing in an individual Olympic event (athlete-events).
+2.  OLYMPICS_HISTORY_NOC_REGIONS table contains NOCs (National Olympic Committees) information and the country it belongs to.
+    Data from noc_regions.csv file is loaded into this table.
+*/
+
+
+
+
+
 DROP TABLE IF EXISTS OLYMPICS_HISTORY;
 CREATE TABLE IF NOT EXISTS OLYMPICS_HISTORY
 (
